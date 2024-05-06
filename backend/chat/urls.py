65844ddb0fca1df90b74e9ad2fbc9b429f71436f
vaanskii <giorgivanadze03@gmail.com>
2024-05-07 01:27:09 +0400

    first commit

---
+++ backend/chat/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'chat'
+
+urlpatterns = [
+    path('api/create-room/<str:uuid>/', views.create_room ,name='create-room'),
+    path('chat-admin/', views.admin, name="admin"),
+    path('chat-admin/<str:uuid>/', views.room, name="room")
+]
---
+++ backend/chat/views.py
@@ -0,0 +1,40 @@
+import json
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+
+from .models import Room
+from account.models import User
+
+@require_POST
+def create_room(request, uuid):
+    name = request.POST.get('name', '')
+    url = request.POST.get('url', '')
+
+    Room.objects.create(uuid=uuid, client=name, url=url)
+
+    return JsonResponse({'message': 'room created'})
+
+@login_required
+def admin(request):
+    rooms = Room.objects.all()
+    users = User.objects.filter(is_staff=True)
+
+    return render(request, 'chat/admin.html', {
+        'rooms': rooms,
+        'users': users
+    })
+
+@login_required
+def room(request, uuid):
+    room = Room.objects.get(uuid=uuid)
+
+    if room.status == Room.WAITING:
+        room.status = Room.ACTIVE
+        room.agent = request.user
+        room.save()
+
+    return render(request, 'chat/room.html', {
+        'room': room
+    })
